bbcf019b0d71c816fe9cde40bd494a86a2da4cc3
Kristen Cignavitch <kcignavitch@gmail.com>
2016-09-05 20:19:26 -0400

    initialize view for prop listings page

--- props/views.py
+++ props/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Props
 
 # Create your views here.
+def props_list(request):
+    props = Props.objects.order_by('-published_date')
+    return render(request, 'props/props_list.html', {'props':props})
